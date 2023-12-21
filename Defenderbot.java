306ed638ca7343222ebeea8f6fa31a8d7bd60a2e
Hugo Nascimento <136537871+hugosanths@users.noreply.github.com>
2023-12-20 22:39:28 -0300

    Add files via upload

---
+++ Defenderbot.java
@@ -0,0 +1,52 @@
+package RoboDefender;
+import robocode.*;
+import java.awt.Color;
+
+
+
+public class Defenderbot extends Robot{
+    double safeDistance = 300;
+	
+	public void run() {
+	
+		setColors(Color.red,Color.blue,Color.green,Color.yellow,Color.blue); 
+
+		while(true) {
+		
+			ahead(100);
+			turnGunRight(180);
+			back(100);
+			turnGunRight(180);
+			turnRadarRight(360);
+			ahead(100);
+		}
+	}
+
+
+	public void onScannedRobot(ScannedRobotEvent e) {
+
+	  //Define a distância considerada segura(por exemplo, 300 pixels)
+	  double safeDistance = 300;
+		         turnGunRight(180);
+		          fire(1);
+				  
+	
+	
+}
+	
+	public void onHitByBullet(HitByBulletEvent e) {
+
+		back(50);
+	}
+	
+
+	public void onHitWall(HitWallEvent e) {
+	
+		back(50);
+	}
+   public void adjustGun(double bearing) {
+        double gunTurn = getHeading() + bearing - getGunHeading();
+           turnGunRight(gunTurn);
+    }
+
+}
